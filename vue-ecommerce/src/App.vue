dc66b774dec05328c35879f78518f46d2719bffc
khoahoang555 <khoahoangyahoo@gmail.com>
2023-03-04 22:41:57 +0700

    Create structure project

---
+++ vue-ecommerce/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HomeView />
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import HomeView from "@/views/HomeView.vue";
+
+export default defineComponent({
+  components: {
+    HomeView
+  }
+});
+</script>
---
+++ vue-ecommerce/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is Home Page</h1>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({});
+</script>
